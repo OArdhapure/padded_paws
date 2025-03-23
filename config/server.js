7fb23be2bdafd027fd310f9610ad2c68e0b3a219
omkar ardhapure <ardhapureomkar@gmail.com>
2025-03-23 10:44:17 -0400

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('RENDER_EXTERNAL_URL'), // Render sets this automatically
   app: {
-    keys: env.array('APP_KEYS', ['yourSuperSecretKey1', 'yourSuperSecretKey2']),
+    keys: env.array('APP_KEYS'),
   },
 });
